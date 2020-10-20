dea4bedd526bc240e2a10a292eb7fdbff3cc6973
like <like@adtech.com.cn>
2020-10-20 11:54:14 +0800

    用flow重构

--- app/src/main/java/com/like/retrofit/sample/MainActivity.kt
+++ app/src/main/java/com/like/retrofit/sample/MainActivity.kt
@@ -167,9 +167,9 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     fun download(view: View) {
-        val url = "https://imtt.dd.qq.com/16891/apk/91059321573A1E1BFF5BC3235A9ABC35.apk"//大文件
-//        val url = "https://imtt.dd.qq.com/16891/apk/8409D55AE4A1DB11320E466C427FD2E2.apk"//小文件
-        downloadJob = GlobalScope.launch(Dispatchers.Main) {
+//        val url = "https://imtt.dd.qq.com/16891/apk/91059321573A1E1BFF5BC3235A9ABC35.apk"//大文件
+        val url = "https://imtt.dd.qq.com/16891/apk/8409D55AE4A1DB11320E466C427FD2E2.apk"//小文件
+        downloadJob = lifecycleScope.launch(Dispatchers.Main) {
             MyApplication.mDownloadRetrofit.download(
                 url,
                 File(cacheDir, "a.apk"),
