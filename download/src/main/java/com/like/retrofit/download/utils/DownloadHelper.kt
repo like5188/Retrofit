697ec3886c0c1b890dd4de4e0c62d3d9d8e3bc52
like5188 <260864866@qq.com>
2021-08-13 10:19:53 +0800

    完善

--- download/src/main/java/com/like/retrofit/download/utils/DownloadHelper.kt
+++ download/src/main/java/com/like/retrofit/download/utils/DownloadHelper.kt
@@ -1,11 +1,11 @@
+@file:Suppress("BlockingMethodInNonBlockingContext")
+
 package com.like.retrofit.download.utils
 
 import com.like.retrofit.download.model.DownloadInfo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
 import retrofit2.HttpException
 import retrofit2.Retrofit
 import java.io.File
@@ -39,6 +39,7 @@ object DownloadHelper {
         // 开始下载
         val response = retrofit.create(DownloadApi::class.java)
             .downloadFile(url, splitFileInfo.getRangeHeader())
+
         // 处理返回数据
         if (response.isSuccessful) {
             if (response.code() == 204) {// 204 No content，表示请求成功，但没有资源可返回。
@@ -51,37 +52,27 @@ object DownloadHelper {
                 it.absolutePath = splitFileInfo.filePath
                 it.totalSize = splitFileInfo.totalSize
             }
-            saveBodyToFile(this, downloadInfo, responseBody)
+
+            // 把 ResponseBody 中的内容存储到 File中
+            responseBody.byteStream().use { inputStream ->
+                RandomAccessFile(File(downloadInfo.absolutePath), "rwd")
+                    .apply { seek(downloadInfo.cachedSize) }
+                    .use { outputStream ->
+                        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+                        var bytesRead = inputStream.read(buffer)
+                        while (bytesRead >= 0) {
+                            outputStream.write(buffer, 0, bytesRead)
+
+                            downloadInfo.status = DownloadInfo.Status.STATUS_RUNNING
+                            emit(downloadInfo)
+
+                            bytesRead = inputStream.read(buffer)
+                        }
+                    }
+            }
         } else if (response.code() != 416) {// 416表示请求的range超出范围。就表示已经下载完成了。不知道为什么，416错误有时候不能触发。难道是因为服务端不支持？
             throw HttpException(response)
         }
-    }
-
-    /**
-     * 把 ResponseBody 中的内容存储到 File中
-     */
-    @Suppress("BlockingMethodInNonBlockingContext")
-    private suspend fun saveBodyToFile(
-        flowCollector: FlowCollector<DownloadInfo>,
-        downloadInfo: DownloadInfo,
-        body: ResponseBody
-    ) = withContext(Dispatchers.IO) {
-        body.byteStream().use { inputStream ->
-            RandomAccessFile(File(downloadInfo.absolutePath), "rwd")
-                .apply { seek(downloadInfo.cachedSize) }
-                .use { outputStream ->
-                    val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
-                    var bytesRead = inputStream.read(buffer)
-                    while (bytesRead >= 0) {
-                        outputStream.write(buffer, 0, bytesRead)
-
-                        downloadInfo.status = DownloadInfo.Status.STATUS_RUNNING
-                        flowCollector.emit(downloadInfo)
-
-                        bytesRead = inputStream.read(buffer)
-                    }
-                }
-        }
-    }
+    }.flowOn(Dispatchers.IO)
 
 }
